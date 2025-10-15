6e9681d15c5d14d0254051a72fbedd9fc487ddb9
ChangHyun Park <buschanny@gmail.com>
2025-10-15 14:50:45 +0900

    feat : String separator

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,31 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = Console.readLine();
+
+        char[] separators = findCustomSeparator(input);
+
+        String[] numbers = separateString(input, separators);
+
+        System.out.println("결과 : " + getSum(numbers, 0));
+    }
+
+    public static String[] separateString(String target, char[] separators){
+        for (char separator : separators){
+            target = target.replace(separator, ' ');
+        }
+        return target.split(" ");
+    }
+
+    public static char[] findCustomSeparator(String target){
+
+        return null;
+    }
+
+    public static int getSum(String[] numbers, int sum){
+
+        return sum;
     }
 }
