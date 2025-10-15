0253eaced9d3e01d35986aa6fc88c22936830bf1
ChangHyun Park <buschanny@gmail.com>
2025-10-15 16:39:40 +0900

    feat : implement custom separator function

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,27 +1,44 @@
 package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Application {
     public static void main(String[] args) {
         String input = Console.readLine();
 
-        char[] separators = findCustomSeparator(input);
+        ArrayList<String> separators = new ArrayList<String>();
+        separators.add(":");
+        separators.add(",");
 
-        String[] numbers = separateString(input, separators);
+        String customStr = findCustomSeparator(input, separators);
+
+        String[] numbers = separateString(customStr, separators);
 
         System.out.println("결과 : " + getSum(numbers, 0));
     }
 
-    public static String[] separateString(String target, char[] separators){
-        for (char separator : separators){
-            target = target.replace(separator, ' ');
+    public static String[] separateString(String target, ArrayList<String> separators){
+        for (String separator : separators){
+            target = target.replace(separator, " ");
         }
         return target.split(" ");
     }
 
-    public static char[] findCustomSeparator(String target){
+    public static String findCustomSeparator(String target, ArrayList<String> separators){
+        int start = target.indexOf("//");
+        int end = target.indexOf("\\n");
 
-        return null;
+        while (start != -1 && end != -1){
+            separators.add(String.valueOf(target.charAt(start + 2)));
+            target = target.substring(0, start) + target.substring(end + 2);
+            start = target.indexOf("//");
+            end = target.indexOf("\\n");
+        }
+
+        return target;
     }
 
     public static int getSum(String[] numbers, int sum){
